79147d13935b78c78c82b9f5a57caef5753e49d7
Marko Viitanen <marko.viitanen@familysearch.org>
2016-12-09 15:33:34 -0700

    Rename code samples by section

---
+++ code/1_9_enums/src/org/familysearch/viitanenm/MyException.java
@@ -0,0 +1,41 @@
+package org.familysearch.viitanenm;
+
+/**
+ * A specialized Exception that also captures the error code
+ */
+public class MyException extends Exception {
+
+  private int errorCode;
+
+  public MyException(int errorCode) {
+    this.errorCode = errorCode;
+  }
+
+  public MyException(Throwable cause, int errorCode) {
+    super(cause);
+    this.errorCode = errorCode;
+  }
+
+  public MyException(String message, int errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+
+  public MyException(String message, Throwable cause, int errorCode) {
+    super(message, cause);
+    this.errorCode = errorCode;
+  }
+
+  public int getErrorCode() {
+    return errorCode;
+  }
+
+  public void setErrorCode(int errorCode) {
+    this.errorCode = errorCode;
+  }
+
+  @Override
+  public String getMessage() {
+    return super.getMessage() +" ("+this.getErrorCode()+")";
+  }
+}
