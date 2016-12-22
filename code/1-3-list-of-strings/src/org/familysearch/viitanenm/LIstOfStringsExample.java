e16c49e9b68d8449660d89cffc5da972646b0c22
Marko Viitanen <marko.viitanen@familysearch.org>
2016-12-22 14:57:07 -0700

    Fix appendix and toc

--- code/1-3-list-of-strings/src/org/familysearch/viitanenm/LIstOfStringsExample.java
+++ code/1-3-list-of-strings/src/org/familysearch/viitanenm/LIstOfStringsExample.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class LIstOfStringsExample {
+public class ListOfStringsExample {
   public static NumberFormat fmt = NumberFormat.getNumberInstance();
 
   public static void main(String[] args) {
